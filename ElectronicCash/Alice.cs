47c23759f9967f91b2a4cfaa10a77185c7d06e2a
Sean Turner <sturner@nmt.edu>
2015-05-13 23:13:01 -0600

    Add the customer actor

---
+++ ElectronicCash/Alice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectronicCash
+{
+    /// <summary>
+    /// The customer
+    /// </summary>
+    public class Alice : BaseActor
+    {
+        
+    }
+}
