e059f0c9d7a078df5ce5552df26b51ec9b3d6482
Sean Turner <sturner@nmt.edu>
2015-05-14 01:14:51 -0600

    Add unit tests for customer actor

---
+++ ElectronicCash.Tests/AliceTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ElectronicCash.Tests
+{
+    /// <summary>
+    /// Tests for the customer actor (alice)
+    /// </summary>
+    [TestFixture]
+    class AliceTests
+    {
+        //static readonly Guid actorGuid = new Guid();
+        readonly Alice _testActor = new Alice("Jane Doe", 4, new Guid(), "JaneDoe;10Downing");
+        [Test]
+        public void DummyTest()
+        {
+            Assert.True(true);
+        }
+
+        [Test]
+        public void OnConstructionNoAlicePropertiesShouldBeNull()
+        {
+            Assert.IsNotNull(_testActor.Name);
+            Assert.IsNotNull(_testActor.NumOrders);
+            Assert.IsNotNull(_testActor.Amount);
+            Assert.IsNotNull(_testActor.ActorGuid);
+            Assert.IsNotNull(_testActor.PersonalData);
+            Assert.IsNotNull(_testActor.PersonalDataBytes);
+            Assert.IsNotNull(_testActor.Ledger);
+        }
+
+        [Test]
+        public void CreateMoneyOrders_AmountShouldEqualGivenAmount()
+        {
+            _testActor.CreateMoneyOrders(500);
+
+            Assert.AreEqual(500, _testActor.Amount);
+        }
+    }
+}
