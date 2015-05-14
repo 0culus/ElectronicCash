1a9a93542ffd90af0e63f67f174572e9faf133d8
Sean Turner <sturner@nmt.edu>
2015-05-13 23:13:38 -0600

    Add separate facility for bit commitment

---
+++ BitCommitment/BitCommitmentEngine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitCommitment
+{
+    /// <summary>
+    /// A class to perform bit commitment. It does not care what the input is; it's just a 
+    /// facility for exchanging bit commitment messages.
+    /// </summary>
+    public class BitCommitmentEngine
+    {
+        #region properties
+
+        public Byte[] BobRandBytesR { get; set; }
+        public Byte[] AliceEncryptedMessage { get; set; }
+
+        #endregion
+    }
+}
