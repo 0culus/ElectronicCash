3837db0807c8a87e0ce516aac77745ff45bd938c
Sean Turner <sturner@nmt.edu>
2015-05-14 00:37:36 -0600

    refactor

---
+++ ElectronicCash.Tests/HelperTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ElectronicCash.Tests
+{
+    [TestFixture]
+    internal class HelperTests
+    {
+        [Test]
+        public void DummyTest()
+        {
+            Assert.True(true);
+        }
+
+        [Test]
+        public void GetBytesGivenString_ShouldYieldNonNullByteArray()
+        {
+            string test = "ThisIsAnAwesomeString";
+            byte[] result = Helpers.GetBytes(test);
+
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void GetStringGivenBytes_ShouldYieldNonNullString()
+        {
+            var rnd = new Random();
+            var testBytes = new byte[10];
+            string result = Helpers.GetString(testBytes);
+
+            Assert.IsNotNull(result);
+        }
+
+        [STAThread]
+        private static void Main()
+        {
+        }
+    }
+}
