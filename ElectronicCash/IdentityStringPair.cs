0cf59fcff988a090eb3aee3f76f702210cfae7a5
Sean Turner <sturner@nmt.edu>
2015-05-13 23:12:36 -0600

    add string pair class from old version

---
+++ ElectronicCash/IdentityStringPair.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace e_cash
+{
+    [Serializable]
+    public struct IdentityStringPair<T> : IEnumerable<T>
+    {
+        private readonly T _left;
+        private readonly T _right;
+
+        public IdentityStringPair(T left, T right)
+        {
+            _left = left;
+            _right = right;
+        }
+
+        public T Left
+        {
+            get { return _left; }
+        }
+
+        public T Right
+        {
+            get { return _right; }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            yield return Left;
+            yield return Right;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
