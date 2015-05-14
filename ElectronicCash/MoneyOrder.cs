36637737f721862a30e283ec098499f0aa452ee5
Sean Turner <sturner@nmt.edu>
2015-05-13 23:13:18 -0600

    Add money order
    This isn't a human actor

---
+++ ElectronicCash/MoneyOrder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectronicCash
+{
+    /// <summary>
+    /// holds the data for a money order
+    /// </summary>
+    public class MoneyOrder
+    {
+        public Byte[] UniquenessString { get; set; }
+
+    }
+}
