b9fb29493d0b856447c10753709e75e11a5d0627
Sean Turner <sturner@nmt.edu>
2015-05-13 23:12:51 -0600

    Add base class for all human actors

---
+++ ElectronicCash/BaseActor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectronicCash
+{
+    public abstract class BaseActor
+    {
+        public string Name { get; set; }
+        public Guid ActorGuid { get; set; }
+    }
+}
