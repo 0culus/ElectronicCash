0997077aa63252e211173de5de9cb34322791eec
Sean Turner <sturner@nmt.edu>
2015-06-29 15:25:34 -0600

    Use one way function protocol for bit commitment

--- BitCommitment/BitCommitmentEngine.cs
+++ BitCommitment/BitCommitmentEngine.cs
@@ -4,15 +4,22 @@ namespace BitCommitment
 {
     /// <summary>
     /// A class to perform bit commitment. It does not care what the input is; it's just a 
-    /// facility for exchanging bit commitment messages.
+    /// facility for exchanging bit commitment messages. Based on Bruce Schneier's one-way 
+    /// function method for committing bits
     /// </summary>
     public class BitCommitmentEngine
     {
-        #region properties
+        public byte[] AliceRandBytes1 { get; set; }
+        public byte[] AliceRandBytes2 { get; set; }
+        public byte[] AliceMessageBytesBytes { get; set; }
 
-        public byte[] BobRandBytesR { get; set; }
-        public byte[] AliceEncryptedMessage { get; set; }
+        public BitCommitmentEngine(byte[] one, byte[] two, byte[] messageBytes)
+        {
+            AliceRandBytes1 = one;
+            AliceRandBytes2 = two;
+            AliceMessageBytesBytes = messageBytes;
+        }
 
-        #endregion
+
     }
 }
