e075d05f6a5363d5f892cd00aca5f208d7a40160
Sean Turner <sturner@nmt.edu>
2015-06-30 13:12:58 -0600

    Refactor again to make variables more clearly related to protocol spec

--- BitCommitment/BitCommitmentProvider.cs
+++ BitCommitment/BitCommitmentProvider.cs
@@ -9,19 +9,25 @@ namespace BitCommitment
     /// </summary>
     public class BitCommitmentProvider
     {
-        public byte[] AliceRandBytes1 { get; set; }
-        public byte[] AliceRandBytes2 { get; set; }
-        public byte[] AliceBytesToCommitBytesToCommit { get; set; }
+        public byte[] AliceRandBytesR1 { get; set; }
+        public byte[] AliceRandBytesR2 { get; set; }
+        public byte[] AliceBytesToCommitB { get; set; }
 
-        public BitCommitmentProvider(byte[] randBytes1, 
-            byte[] randBytes2, 
+        public BitCommitmentProvider(byte[] randBytesR1, 
+            byte[] randBytesR2, 
             byte[] bytesToCommit)
         {
-            AliceRandBytes1 = randBytes1;
-            AliceRandBytes2 = randBytes2;
-            AliceBytesToCommitBytesToCommit = bytesToCommit;
+            AliceRandBytesR1 = randBytesR1;
+            AliceRandBytesR2 = randBytesR2;
+            AliceBytesToCommitB = bytesToCommit;
         }
 
+        /// <summary>
+        /// Returns a byte array containing the one way function computed for the triple
+        /// (R_1, R_2, B) where R_1 is AliceRandBytesR1, R_2 is AliceRandBytesR2, and B is the byte(s) 
+        /// Alice wants to commit. 
+        /// </summary>
+        /// <returns></returns>
         public byte[] BitCommitMessage()
         {
             throw new NotImplementedException();
