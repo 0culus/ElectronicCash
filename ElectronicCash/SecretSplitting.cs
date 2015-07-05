5fcb0a04cd45580c82d1a2b180fc6c8f504dbfd2
Sean Turner <sturner@nmt.edu>
2015-07-04 20:37:06 -0600

    Stub out a class for secret splitting

---
+++ ElectronicCash/SecretSplitting.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectronicCash
+{
+    /// <summary>
+    /// This portion of the protocol is critical...Alice splits her bit committed packets so that
+    /// they are not accessible except when the bank requires it. 
+    /// </summary>
+    class SecretSplitting
+    {
+    }
+}
