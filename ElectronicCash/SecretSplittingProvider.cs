f83ccd4d01da8e5659dca1a00a2627f8cb149ffb
Sean Turner <sturner@nmt.edu>
2015-07-06 20:15:34 -0600

    Provide two ctors for different ways of splitting (2 actors or >2 actors)

--- ElectronicCash/SecretSplittingProvider.cs
+++ ElectronicCash/SecretSplittingProvider.cs
@@ -13,13 +13,35 @@ namespace ElectronicCash
     class SecretSplittingProvider
     {
         List<byte[]> RandomBytes { get; set; }
-
+        byte[] RandomOne { get; set; }
+        byte[] RandomTwo { get; set; }
         private byte[] SecretMessage { get; set; }
 
-        public SecretSplittingProvider(byte[] secretMessage, IEnumerable<byte[]> randMessageBytes)
+        /// <summary>
+        /// Splitting between an arbitrary number of actors
+        /// </summary>
+        /// <param name="secretMessage"></param>
+        /// <param name="randMessageBytes"></param>
+        public SecretSplittingProvider(byte[] secretMessage, 
+            IEnumerable<byte[]> randMessageBytes)
         {
             SecretMessage = secretMessage;
             RandomBytes = new List<byte[]>(randMessageBytes);
+        }
+
+        /// <summary>
+        /// Splitting between two actors
+        /// </summary>
+        /// <param name="secretMessage"></param>
+        /// <param name="randomBytesOne"></param>
+        /// <param name="randomBytesTwo"></param>
+        public SecretSplittingProvider(byte[] secretMessage, 
+            byte[] randomBytesOne, 
+            byte[] randomBytesTwo)
+        {
+            SecretMessage = secretMessage;
+            RandomOne = randomBytesOne;
+            RandomTwo = randomBytesTwo;
         }
     }
 }
