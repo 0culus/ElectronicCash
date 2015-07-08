fdb5608581b538c9d16c0801b7b16600645d0516
Sean Turner <sturner@nmt.edu>
2015-07-07 23:53:45 -0600

    Add test fixture for secret splitting

---
+++ ElectronicCash.Tests/SecretSplittingTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ElectronicCash.Tests
+{
+    [TestFixture]
+    class SecretSplittingTests
+    {
+        private static readonly byte[] _message = Helpers.GetBytes("Supersecretmessage");
+        private static readonly byte[] _randBytes = Helpers.GetRandomBytes(Helpers.GetString(_message).Length * sizeof(char));
+        private readonly SecretSplittingProvider _splitter = new SecretSplittingProvider(_message, _randBytes);
+
+        [Test]
+        public void OnSplit_OriginalMessageShouldBeRecoverable()
+        {
+            _splitter.SplitSecretBetweenTwoPeople();
+
+            var r = _splitter.R;
+            var s = _splitter.S;
+
+            var m = Helpers.ExclusiveOr(r, s);
+
+            Assert.AreEqual(Helpers.GetString(m), Helpers.GetString(_message));
+        }
+    }
+}
