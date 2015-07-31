f069be2b70dcce4bfd244ad2dcd424f39bb5de09
Sean Turner <sturner@nmt.edu>
2015-07-30 22:51:27 -0600

    ReSharper remove unused directives in solution

--- SecretSplitting/SecretSplittingProvider.cs
+++ SecretSplitting/SecretSplittingProvider.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.CodeDom;
 using System.Collections.Generic;
-using System.Dynamic;
-using System.Runtime.CompilerServices;
 using System.Security.Cryptography;
 
 namespace SecretSplitting
